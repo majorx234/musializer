365bd9b9c4ef54b482de11cce32ec34b89ea2dde
MajorX234 <majorx234@googlemail.com>
2024-10-26 17:59:21 +0200

    Add comparism between possible wrong division notation

--- src/sine_wave_compare.c
+++ src/sine_wave_compare.c
@@ -20,15 +20,26 @@ void updateSignal2(float* signal, float frequency, float sample_duration) {
 }
 
 
-
 void main(void){
   float signal1[1024] = {0};
   float signal2[1024] = {0};
 
+  printf("==== compar 2 functions ==== \n\n");
   updateSignal1(signal1,100,1.0/STREAM_BUFFER_SIZE);
   updateSignal2(signal2,100,1.0/STREAM_BUFFER_SIZE);
 
-  for ( int i = 0;i<1024;i++){
-    printf("sig1[%d] %.3f,    sig2[%d]: %.3f\n",i, signal1[i],i, signal2[i]);
+  for ( int i = 0;i<64;i++){
+    printf("normal: sig1[%d] %.3f,    sig2[%d]: %.3f\n",i, signal1[i],i, signal2[i]);
   }
+
+  printf("\n\n");
+  printf("==== compar 2 functions with (int/int vs float/int) ==== \n\n");
+
+  updateSignal1(signal1,100,48000/STREAM_BUFFER_SIZE);
+  updateSignal2(signal2,100,48000.0/STREAM_BUFFER_SIZE);
+
+  for ( int i = 0;i<64;i++){
+    printf("int/int vs Float/int: sig1[%d] %.3f,    sig2[%d]: %.3f\n",i, signal1[i],i, signal2[i]);
+  }
+
 }
