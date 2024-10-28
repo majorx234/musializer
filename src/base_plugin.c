e8f6cd6c37f836622e5f22f98ee727213d6c052c
MajorX234 <majorx234@googlemail.com>
2024-10-28 01:19:49 +0100

    Add base plugin

---
+++ src/base_plugin.c
@@ -0,0 +1,25 @@
+#include "visual_plugin.h"
+#include <stdlib.h>
+#include <stdbool.h>
+#include <raylib.h>
+
+typedef struct BasePlugin {
+  bool started;
+} BasePlugin;
+
+void* visual_plugin_init(void) {
+  BasePlugin* plugin = malloc(sizeof(BasePlugin));
+  plugin->started = false;
+  return plugin;
+}
+
+void visual_plugin_pre_reload(void* plugin) {}
+
+void visual_plugin_post_reload(void* plugin){}
+
+void visual_plugin_update(void *plugin) {}
+
+void visual_plugin_load_files(FilePathList file_path_list) {}
+
+void visual_plugin_close(void* plugin) {
+}
